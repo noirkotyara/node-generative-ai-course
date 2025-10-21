3875996d41e129c5a867ee01acd9fecb128f5c60
Diana Maliukh <noirkotyara@gmail.com>
2025-10-21 14:17:59 +0300

    Try to use tokenizer for prompt

--- basics/src/index.ts
+++ basics/src/index.ts
@@ -1,17 +1,34 @@
 import { OpenAI } from 'openai'
+import { encoding_for_model } from 'tiktoken'
 
 const openAI = new OpenAI();
+const model = 'gpt-5';
 
 async function main() {
     const response = await openAI.chat.completions.create({
-        model: 'gpt-4o',
+        model,
         messages: [{
+            role: "system",
+            content: "Respond as I am a princess Diana"
+        }, {
             role: 'user',
             content: 'What is Mount Hoverla\'s height?'
-        }]
+        }],
+        n: 2,
     })
 
-    console.info('[chat:response]', response.choices[0].message.content)
+    response.choices.forEach((choice, idx) => {
+        console.info(`[main:chat-response-${idx}]`, choice.message.content)
+    })
 }
+
+function encodePrompt() {
+    const prompt = 'What is Mount Hoverla\'s height?'
+    const encoder = encoding_for_model(model)
+    const words = encoder.encode(prompt)
+    console.info('[encodePrompt:encoded-prompt]', words)
+}
+
+encodePrompt()
 
 main()
